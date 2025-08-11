2f9ad28c41635580291cc663bdaf65a0e0c4712e
vastvik8051 <vastviksharma8051@gmail.com>
2025-08-11 17:31:48 +0000

    data types notes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // this treats all code as newer version of code in javascript
+
+// alert('hellow') using node js not browser
+// code should always be clean an readable
+
+console.log("Vastvik")
+const name = "Vastvik"
+let age = 20
+let isLoggedIn = false // boolean data type (boolean are true or false true = 1, false 0)
+
+/*
+number => 2 to 52 values
+bigint => it is used for big values
+string => double quotes"" or single quotes''
+null => used for standalone values
+undefined => when there is no value assigned
+symbol => it is used to create unique values
+object => it is used to store collection of data and more complex entities
+
+    */
