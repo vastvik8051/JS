ef30ff25575a25ef402c9f6c097ce3ba07fbdefc
vastvik8051 <vastviksharma8051@gmail.com>
2025-08-11 14:58:16 +0000

    learning variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "forexample@anymail.com"
+var accountPassword = "12345"
+accountCity = "gwalior"
+let accountState = "Madhya Pradesh"
+
+// account_Id = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "123"
+accountCity = "bhopal"
+
+/*
+do not use var because it has issues with jawa only use let
+*/
+console.log(accountId);
+
+console.table({accountId, accountEmail, accountCity, accountState, accountPassword})
