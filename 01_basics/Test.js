d95cef737c91a6718633ebe38550815acc794923
vastvik8051 <vastviksharma8051@gmail.com>
2025-08-14 17:04:48 +0000

    just testing what i learned

--- 01_basics/Test.js
+++ 01_basics/Test.js
@@ -1,1 +1,21 @@
-console.log("Vastvik")
+//console.log("Vastvik")
+
+// recalling heap memory
+
+let user1 = {
+    name: "vastvik",
+    payment: "UPI",
+    email: "abcd@Mail.com",
+    age: 24,
+    isLoggedIn: "true",     
+}
+
+// in heap, values can be recalled in any order.
+//console.log(user1)
+//console.log(user1.age)
+
+
+/* 
+1. Heap is used for non-primitive data types like objects and arrays, which represent large amounts of data.
+2. Stack is used for primitive data types like numbers, strings, and booleans, which represent small amounts of data.
+*/
